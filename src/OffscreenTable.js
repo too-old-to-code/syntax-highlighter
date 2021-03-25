89df5947109143d532ce44035aaa8a835da5b9f1
Andrew Trigg <andrewt51@hotmail.com>
2021-03-25 22:15:42 +0000

    Change ref to actual table

--- src/OffscreenTable.js
+++ src/OffscreenTable.js
@@ -4,13 +4,12 @@ import React from "react";
 export const OffscreenTable = React.forwardRef(({ fontSize }, ref) => {
   return (
     <div
-      ref={ref}
       className="offscreen"
       style={{
         fontSize: `${fontSize}pt`,
       }}
     >
-      <table>
+      <table ref={ref}>
         <tbody>
           <tr>
             <td></td>
